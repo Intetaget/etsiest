a983bc8ca537d29459d7ea134f61d74fba09dfe0
Mans Nilsson <intetaget>
2015-10-26 16:55:11 -0400

    Add etsy and sinatra to the Gemfile. Define our controller.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in etsiest.gemspec
 gemspec
+
+gem "etsy"
+gem "pry"
+gem "sinatra"
--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -1,5 +1,20 @@
+require 'pry'
+require "sinatra/base"
 require "etsiest/version"
+require "etsy"
 
 module Etsiest
-  # Your code goes here...
+ Etsy.api_key = ENV["ETSY_KEY"]
+ 
+	class App < Sinatra::Base
+
+ 		get "/" do
+      		response=Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => 'whiskey')
+		  	response = response.result
+		  	binding.pry
+		  	erb :index, locals: { results: response}
+      	end
+
+    	run! if app_file == $0
+    end
 end
