57226f817f8d572b1f6dc2dcc03ac48b84f0a3f6
Mans Nilsson <intetaget>
2015-10-27 14:25:34 -0400

    Get the page up and running.

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -3,16 +3,15 @@ require "sinatra/base"
 require "etsiest/version"
 require "etsy"
 
+Etsy.api_key = ENV["ETSY_KEY"]
+
 module Etsiest
- Etsy.api_key = ENV["ETSY_KEY"]
  
 	class App < Sinatra::Base
 
  		get "/" do
       		response=Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => 'whiskey')
-		  	response = response.result
-		  	binding.pry
-		  	erb :index, locals: { results: response}
+		  	erb :index, locals: { results: response.result }
       	end
 
     	run! if app_file == $0
